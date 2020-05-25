02311c9bfe7bca15333fea9b4f360c1a5d8f8679
matiri132 <matiasmachado7@gmail.com>
2020-05-25 15:30:51 -0300

    nginx

--- installWSGI2.sh
+++ installWSGI2.sh
@@ -3,6 +3,8 @@ echo "SETING VARS..."
 APPDIR="/home/$USER/webapps"
 APPNAME='myapp'
 HOST_NAME='localhost'
+SERVERNAME='localhost'
+DOMAINNAME='localhost'
 VENV_NAME='appVenv'
 WD=$(pwd)
 
@@ -15,6 +17,11 @@ sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidservice
 sed -i "s/APPDIR/\/home\/$USER\/webapps/g" ${WD}/ssidservice
 sed -i "s/USER/$USER/g" ${WD}/ssidservice
 sed -i "s/VENVNAME/${VENV_NAME}/g" ${WD}/ssidservice
+
+sed -i "s/SERVERNAME/${SERVERNAME}/g" ${WD}/ssidnginx
+sed -i "s/DOMAINNAME/${DOMAINNAME}/g" ${WD}/ssidnginx
+sed -i "s/APPDIR/\/home\/$USER\/webapps/g" ${WD}/ssidnginx
+sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidnginx
 
 #sed -i "s/USER/$USER/g" ${WD}/appSv
 #sed -i "s/MYAPP/${APPNAME}/g" ${WD}/appSv
@@ -51,12 +58,22 @@ pip install wheel
 pip install uwsgi flask
 deactivate
 
-sudo cp ${WD}/ssidservice /etc/systemd/system/${APPNAME}.service
+#Install application
 cp ${WD}/ssidappini ${APPDIR}/${APPNAME}.ini
 cp ${WD}/ssidapp.py ${APPDIR}/ssidapp.py
 cp ${WD}/wsgi.py ${APPDIR}/wsgi.py
 
+#Creating SERVICE
+sudo cp ${WD}/ssidservice /etc/systemd/system/${APPNAME}.service
 sudo systemctl start ${APPNAME}
 sudo systemctl enable ${APPNAME}
 
+#Configure NGINX
+sudo cp ${WD}/ssidnginx /etc/nginx/sites-available/${APPNAME}
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+if [ ! -x "$(command -v ufw))" ]; then
+	echo "Configure firewall..."
+	sudo ufw allow 'Nginx Full'
+fi
 
