662fa3f58ef111abe3e0d7cb5d166aa069c80ccf
matiri132 <matiasmachado7@gmail.com>
2020-05-26 19:24:29 -0300

    Add uninstall WSGI

--- installWSGI.sh
+++ installWSGI.sh
@@ -6,70 +6,95 @@ HOST_NAME='0.0.0.0'
 DOMAINNAME='localhost'
 WD=$(pwd)
 
-#Preparing config files
-sed -i "s/HOSTNAME/${HOST_NAME}/g" ${WD}/ssidapp.py
-
-sed -i "s/APPDIR/\/home\/$USER\/webapps/g" ${WD}/ssidappini
-sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidappini
-
-sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidservice
-sed -i "s/APPDIR/\/home\/$USER\/webapps/g" ${WD}/ssidservice
-sed -i "s/USER/$USER/g" ${WD}/ssidservice
-
-sed -i "s/DOMAINNAME/${DOMAINNAME}/g" ${WD}/ssidnginx
-sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidnginx
-
-
-echo "INSTALLING DEPENDENCIES..."
-#Install dependencies
-#Python dep
-if [ ! -x "$(command -v python3)" ]; then
-	echo "Installing Python3..."
-	sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv 
-fi
-#Nginx dep
-if [ ! -x "$(command -v nginx))" ]; then
-	echo "Installing NGINX..."
-	sudo apt install nginx
-fi
-#Sed omstaññ
-if [ ! -x "$(command -v sed))" ]; then
-	echo "Installing SED..."
-	sudo apt install sed
-fi
-
-echo "INSTALL Python PACKAGES..."
-sudo pip install wheel flask
-sudo pip install uwsgi
-
-echo "SETUP FLASK APP..."
-if [[ ! -d ${APPDIR} ]]
+if[ $1 = "install"]
 then
-	mkdir ${APPDIR}
-	sudo chown www-data ${APPDIR}
+	#Preparing config files
+	sed -i "s/HOSTNAME/${HOST_NAME}/g" ${WD}/ssidapp.py
+
+	sed -i "s/APPDIR/\/home\/$USER\/webapps/g" ${WD}/ssidappini
+	sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidappini
+
+	sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidservice
+	sed -i "s/APPDIR/\/home\/$USER\/webapps/g" ${WD}/ssidservice
+	sed -i "s/USER/$USER/g" ${WD}/ssidservice
+
+	sed -i "s/DOMAINNAME/${DOMAINNAME}/g" ${WD}/ssidnginx
+	sed -i "s/APPNAME/${APPNAME}/g" ${WD}/ssidnginx
+
+
+	echo "INSTALLING DEPENDENCIES..."
+	#Install dependencies
+	if[ $2 = "full"]
+	then
+		install_packages();
+	fi
+
+	echo "SETUP FLASK APP..."
+	if [[ ! -d ${APPDIR} ]]
+	then
+		mkdir ${APPDIR}
+		chown www-data ${APPDIR}
+	fi
+
+	#Install application
+	cp ${WD}/ssidappini ${APPDIR}/${APPNAME}.ini
+	cp ${WD}/ssidapp.py ${APPDIR}/ssidapp.py
+	cp ${WD}/wsgi.py ${APPDIR}/wsgi.py
+
+	#Creating SERVICE
+	cp ${WD}/ssidservice /etc/systemd/system/${APPNAME}.service
+	systemctl start ${APPNAME}
+	systemctl enable ${APPNAME}
+
+	#Configure NGINX
+	rm /etc/nginx/sites-enabled/default
+	cp ${WD}/ssidnginx /etc/nginx/sites-available/${APPNAME}
+	ln -s /etc/nginx/sites-available/${APPNAME} /etc/nginx/sites-enabled/
+
+	if [ -x "$(command -v ufw))" ]; then
+		echo "Configure firewall..."
+		ufw allow 'Nginx Full'
+	fi
+
+	systemctl restart nginx
 fi
 
-#Install application
-sudo cp ${WD}/ssidappini ${APPDIR}/${APPNAME}.ini
-sudo cp ${WD}/ssidapp.py ${APPDIR}/ssidapp.py
-sudo cp ${WD}/wsgi.py ${APPDIR}/wsgi.py
-
-
-
-#Creating SERVICE
-sudo cp ${WD}/ssidservice /etc/systemd/system/${APPNAME}.service
-sudo systemctl start ${APPNAME}
-sudo systemctl enable ${APPNAME}
-
-#Configure NGINX
-sudo rm /etc/nginx/sites-enabled/default
-sudo cp ${WD}/ssidnginx /etc/nginx/sites-available/${APPNAME}
-sudo ln -s /etc/nginx/sites-available/${APPNAME} /etc/nginx/sites-enabled/
-
-if [ -x "$(command -v ufw))" ]; then
-	echo "Configure firewall..."
-	sudo ufw allow 'Nginx Full'
+if[ $1 = "uninstall"]
+then
+	#Remove WEBAPP	
+	rm -R ${APPDIR}
+	#Remove Service
+	systemctl stop ${APPNAME}
+	systemctl disable ${APPNAME}
+	rm /etc/systemd/system/${APPNAME}.service
+	#Set default nginx
+	rm /etc/nginx/sites-enabled/${APPNAME}
+	rm /etc/nginx/sites-available/${APPNAME}
+	ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+	systemctl reload nginx
 fi
 
-sudo systemctl restart nginx
+install_packages(){
+	#Python dep
+	sudo apt install python3-dev build-essential libssl-dev libffi-dev python3-setuptools 
 
+	#Nginx dep
+	if [ ! -x "$(command -v nginx))" ]; then
+		echo "Installing NGINX..."
+		sudo apt install nginx
+	fi
+	#Sed omstaññ
+	if [ ! -x "$(command -v sed))" ]; then
+		echo "Installing SED..."
+		sudo apt install sed
+	fi
+
+	echo "INSTALL Python PACKAGES..."
+	if [ ! -x "$(command -v pip3))" ]; then
+		echo "Installing PIP..."
+		sudo apt install python3-pip 	
+	fi
+
+	sudo pip3 install wheel flask
+	sudo pip3 install uwsgi
+}
